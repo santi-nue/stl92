3a03ea1203c768bd7f6d7962d36a09d119a1bbcf
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-02-25 13:26:26 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 serve(async (req) => {
   if (req.url === "/") {
-    const imageUrl = "https://cdn.airnavradar.com/airlines/sq/IBE.png";
+    const imageUrl = "https://www.observatorioremoto.com/emadato/temperatura.jpg";
     const response = await fetch(imageUrl);
     const imageData = await response.arrayBuffer();
     return new Response(imageData, {
