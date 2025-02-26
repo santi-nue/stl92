53ccfa0b7e41a26de52acc8f81acfaf91c02b96f
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-02-26 11:50:46 +0100

    Update getimages.py

--- getimages.py
+++ getimages.py
@@ -17,38 +17,38 @@ def fetch_image(url):
         logging.error(f"Error fetching image from {url}: {e}")
         return None
 
-def create_empty_zip(zip_path):
-    with zipfile.ZipFile(zip_path, 'w') as zip_file:
-        # Creating an empty zip file
-        pass
-
-
 def create_zip_from_csv(csv_path, zip_path):
     if not os.path.exists(csv_path):
         logging.error(f"CSV file does not exist: {csv_path}")
         return
-        
+
     logging.info(f"Reading CSV file: {csv_path}")
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
             for row in csv_reader:
-                code = row['Code']
-                url = f"https://cdn.airnavradar.com/airlines/sq/{code}.png"
-                image_data = fetch_image(url)
-                if image_data:
-                    zip_file.writestr(f"{code}.png", image_data)
-                    logging.info(f"Added image for code: {code}")
+                if 'Code' in row:
+                    code = row['Code']
+                    url = f"https://cdn.airnavradar.com/airlines/sq/{code}.png"
+                    image_data = fetch_image(url)
+                    if image_data:
+                        zip_file.writestr(f"{code}.png", image_data)
+                        logging.info(f"Added image for code: {code}")
+                else:
+                    logging.warning("Code column not found in CSV row")
         with open(zip_path, 'wb') as f:
             f.write(zip_buffer.getvalue())
         logging.info("ZIP file created successfully.")
+
+def create_empty_zip(zip_path):
+    with zipfile.ZipFile(zip_path, 'w') as zip_file:
+        # Creating an empty zip file
+        pass
 
 if __name__ == "__main__":
     csv_path = 'airlines.csv'
     zip_path = 'airline_images.zip'
     create_empty_zip(zip_path)
     create_zip_from_csv(csv_path, zip_path)
-
-
-
+    
