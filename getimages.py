8b27db831980a922d601b01be0b6787562e71b7b
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-02-26 12:05:06 +0100

    Update getimages.py

--- getimages.py
+++ getimages.py
@@ -28,7 +28,7 @@ def create_zip_from_csv(csv_path, zip_path):
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
             for row in csv_reader:
-                if 'Code' in row:
+                if '\ufeffCode' in row:
                     code = row['\ufeffCode']
                     url = f"https://cdn.airnavradar.com/airlines/sq/{code}.png"
                     image_data = fetch_image(url)
